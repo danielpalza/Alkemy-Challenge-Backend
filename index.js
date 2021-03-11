29c93538cb4bb8ee567d1476c5a31fb10c7ffec5
danielpalza <51375733+danielpalza@users.noreply.github.com>
2021-03-11 19:04:53 -0300

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const db = require("./database");
 const routes = require("./routes/index");
 
 let corsOptions = {
-  "origin": "https://alkemy-challenge-front.vercel.app",
+  origin: '*',
   preflightContinue: true,
   optionsSuccessStatus: 200, // For legacy browser support
 };
